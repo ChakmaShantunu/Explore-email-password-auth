f4a51c42383274b8689b313415263a91c70fa810
shantunu007 <chakmashantunu.web@gmail.com>
2026-06-22 12:29:43 +0600

    create register form UI with email and password fields

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,13 @@
+import { NavLink } from "react-router";
 
 
 const Navbar = () => {
 
     const links = <>
 
-        <li><a>Item 1</a></li>
-        <li>
-            <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-            </details>
-        </li>
-        <li><a>Item 3</a></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
 
     </>
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,28 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Root from './layout/Root.jsx';
 import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     children: [
+
       {
         index: true,
         Component: Home
+      },
+
+      {
+        path: "/login",
+        Component: Login
+      },
+
+      {
+        path: "/register",
+        Component: Register
       }
     ]
   },
