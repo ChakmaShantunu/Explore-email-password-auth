52319d6ce1ca298e16a87e43c6ceb789ebf99aa7
shantunu007 <chakmashantunu.web@gmail.com>
2026-06-23 11:12:51 +0600

    footer added and firebase install done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -9,56 +9,58 @@ const Register = () => {
         console.log(email, password);
     }
     return (
-        <div>
-            <h2 className="text-2xl">this is register</h2>
-            <form onSubmit={handleRegister}>
+        <div className="max-w-3xl border p-4 mt-12 flex justify-center mx-auto">
+            <div>
+                {/* <h2 className="text-2xl">this is register</h2> */}
+                <form className="space-y-4" onSubmit={handleRegister}>
 
-                {/* email submit */}
-                <div className="join">
-                    <div>
-                        <label className="input validator join-item">
-                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2.5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                >
-                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                                </g>
-                            </svg>
-                            <input type="email" name="email" placeholder="mail@site.com" required />
-                        </label>
-                        <div className="validator-hint hidden">Enter valid email address</div>
-                    </div>
-                    {/* <button className="btn btn-neutral join-item">Register</button> */}
-                </div> <br />
+                    {/* email submit */}
+                    <div className="join">
+                        <div>
+                            <label className="input validator join-item">
+                                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                        strokeWidth="2.5"
+                                        fill="none"
+                                        stroke="currentColor"
+                                    >
+                                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                    </g>
+                                </svg>
+                                <input type="email" name="email" placeholder="mail@site.com" required />
+                            </label>
+                            <div className="validator-hint hidden">Enter valid email address</div>
+                        </div>
+                        {/* <button className="btn btn-neutral join-item">Register</button> */}
+                    </div> <br />
 
-                {/* password submit */}
-                <div className="join">
-                    <div>
-                        <label className="input validator join-item">
-                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2.5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                >
-                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                                </g>
-                            </svg>
-                            <input type="password" name="password" placeholder="password" required />
-                        </label>
-                        <div className="validator-hint hidden">Enter valid email address</div>
-                    </div>
-                </div><br />
-                <input type="submit" className="btn btn-primary" value="Submit" />
-            </form>
+                    {/* password submit */}
+                    <div className="join">
+                        <div>
+                            <label className="input validator join-item">
+                                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                        strokeWidth="2.5"
+                                        fill="none"
+                                        stroke="currentColor"
+                                    >
+                                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                    </g>
+                                </svg>
+                                <input type="password" name="password" placeholder="password" required />
+                            </label>
+                            <div className="validator-hint hidden">Enter valid email address</div>
+                        </div>
+                    </div><br />
+                    <input type="submit" className="btn btn-primary" value="Submit" />
+                </form>
+            </div>
         </div>
     );
 };
